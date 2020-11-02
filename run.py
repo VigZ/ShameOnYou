4640b966b6d7f18458ff7e0c1a471a190b1795f3
vigz <kyle.vigorito@gmail.com>
2020-11-02 14:00:07 -0500

    added virtualenv and requirements

--- run.py
+++ run.py
@@ -24,20 +24,34 @@ async def on_member_join(member):
         f'Hello {member.name}, welcome to {GUILD}! \n My name is ShameBot, let\'s keep eachother accountable, and be productive! \n Remember, I am always watching....'
     )
 
+
 @shame_bot.command(name='shame', help='Shame your fellow group members for not being productive.')
 async def shame_user(ctx, member: discord.Member):
     choice = random.choice(SHAME_QUIPS[shame_bot.intensity])
     response = f'{member.mention} {choice}'
     await ctx.send(response)
 
+@shame_user.error
+async def shame_user_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('I can\'t seem to find that user...')
+
 @shame_bot.command(name='intensity', help='Change the bot\'s intensity 1-5.')
-async def shame_user(ctx, intensity: int):
+async def change_intensity(ctx, intensity: int):
+    if intensity < 0 or intensity > len(BotIntensity):
+        raise commands.BadArgument
+
     shame_bot.intensity = BotIntensity(intensity)
     response = f'Intensity has been changed to {shame_bot.intensity.name}'
     await ctx.send(response)
 
+@change_intensity.error
+async def shame_user_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('Please enter a level between 1 and 5.')
+
 @shame_bot.command(name='currentintensity', help='Gives the current intensity of ShameBot.')
-async def shame_user(ctx,):
+async def current_intensity(ctx):
     response = f'The current intensity is {shame_bot.intensity.name}'
     await ctx.send(response)
 
